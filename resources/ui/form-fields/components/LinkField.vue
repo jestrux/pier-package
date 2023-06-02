1d0e499adc5c79c0d3cff21005f1e110cca190f0
Walter Kimaro <wakyj07@gmail.com>
2023-06-02 15:16:08 +0300

    Default link value to string

--- resources/ui/form-fields/components/LinkField.vue
+++ resources/ui/form-fields/components/LinkField.vue
@@ -133,7 +133,7 @@ export default {
     },
     data() {
         return {
-            val: false,
+            val: "",
             dontUpdate: false,
             previewReady: false,
             error: null
