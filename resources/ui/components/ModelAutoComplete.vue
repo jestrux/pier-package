1ea961f5c243b91f635f33dc05fe8227aee559d4
Walter Kimaro <wakyj07@gmail.com>
2023-06-06 12:14:32 +0300

    Encapsulate model autocomplete logic

---
+++ resources/ui/components/ModelAutoComplete.vue
@@ -0,0 +1,80 @@
+<style>
+.autocomplete-input {
+  padding-left: 48px !important;
+}
+</style>
+<template>
+  <div>
+    <autocomplete
+      ref="autocomplete"
+      :placeholder="placeholder"
+      :search="search"
+      :get-result-value="getResultValue"
+      :debounceTime="300"
+      @submit="handleSubmit"
+    />
+  </div>
+</template>
+
+<script>
+import Autocomplete from "@trevoreyre/autocomplete-vue";
+import "@trevoreyre/autocomplete-vue/dist/style.css";
+
+export default {
+  name: "ModelAutoComplete",
+  props: {
+    model: String,
+    modelMainField: String,
+    value: Object | String,
+    placeholder: String,
+  },
+  inject: ["API"],
+  mounted() {
+    if (this.value) this.val = this.value;
+    this.$refs.autocomplete.setValue(" ");
+  },
+  data() {
+    return {
+      val: false,
+      dontUpdate: false,
+    };
+  },
+  methods: {
+    search(input) {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const results = await this.API.searchModel(this.model, input);
+          resolve(results);
+        } catch (error) {
+          reject("Failed to search for field.");
+          console.log("Failed to search for field.", error);
+        }
+      });
+    },
+    getResultValue(result) {
+      return result.label;
+    },
+    handleSubmit(result) {
+      this.val = result;
+        this.$refs.autocomplete.setValue(" ");
+    },
+  },
+  watch: {
+    value: function (newValue) {
+      this.dontUpdate = true;
+      this.val = newValue;
+    },
+    val: function (newValue) {
+      if (this.dontUpdate) {
+        this.dontUpdate = false;
+        return;
+      }
+
+      if (newValue != null) this.$emit("input", newValue);
+    },
+  },
+  components: {
+    Autocomplete,
+  },
+};
+</script>
--- resources/ui/form-fields/components/ReferenceField.vue
+++ resources/ui/form-fields/components/ReferenceField.vue
@@ -1,8 +1,3 @@
-<style>
-    .autocomplete-input{
-        padding-left: 48px !important;
-    }
-</style>
 <template>
     <div>
         <div :class="{'hidden': !val, 'flex' : val}" class="flex items-center px-2 rounded bg-gray-200 bg-opacity-25 border border-gray-300" style="height: 40px">
@@ -14,20 +9,20 @@
             </button>
         </div>
 
-        <autocomplete :class="{'hidden': val}"
-            ref="autocomplete"
-            :placeholder="`Type to search for ${label}`"
-            :search="search" 
-            :get-result-value="getResultValue"
-            :debounceTime="300"
-            @submit="handleSubmit"
-        />
+        <div ref="autocomplete">
+            <autocomplete
+                v-if="!val"
+                :model="referenceModel"
+                :modelMainField="referenceModelMainField"
+                :placeholder="`Type to search for ${label}`"
+                v-model="val"
+            />
+        </div>
     </div>
 </template>
 
 <script>
-import Autocomplete from '@trevoreyre/autocomplete-vue'
-import '@trevoreyre/autocomplete-vue/dist/style.css'
+import Autocomplete from '../../components/ModelAutoComplete.vue'
 
 export default {
     name: "ReferenceField",
@@ -37,67 +32,40 @@ export default {
         label: String,
         value: Object|String
     },
-    inject: ['API'],
-    mounted() {
-        if(this.value) this.val = this.value;
-        this.$refs.autocomplete.setValue(" ");
-    },
     data() {
         return {
-            val: false,
-            dontUpdate: false
-        }
+            val: "",
+            dontUpdate: false,
+        };
+    },
+    computed: {
+        displayValue() {
+            if (this.val && this.referenceModelMainField) return this.val[this.referenceModelMainField];
+
+            return null;
+        },
     },
     methods: {
-        search(input) {
-            // if (input.length < 1) { return [] }
-
-            return new Promise(async (resolve, reject) => {
-                try {
-                    const results = await this.API.searchModel(this.referenceModel, input);
-                    resolve(results);
-                } catch (error) {
-                    reject("Failed to search for field.");
-                    console.log("Failed to search for field.", error);
-                }
-            });
-        },
         changeValue() {
             this.val = null;
             this.$nextTick(() => {
-                this.$refs.autocomplete.$el.querySelector(".autocomplete-input").focus()
+                this.$refs.autocomplete.querySelector(".autocomplete-input").focus();
             });
         },
-        getResultValue(result) {
-            return result.label;
-        },
-        handleSubmit(result) {
-            this.val = result;
-            this.$refs.autocomplete.setValue(" ");
-        }
-    },
-    computed: {
-        displayValue(){
-            if(this.val && this.referenceModelMainField)
-                return this.val[this.referenceModelMainField];
-
-            return null;
-        }
     },
     watch: {
-        value: function(newValue){
+        value: function (newValue) {
             this.dontUpdate = true;
             this.val = newValue;
         },
-        val: function(newValue){
-            if(this.dontUpdate){
+        val: function (newValue) {
+            if (this.dontUpdate) {
                 this.dontUpdate = false;
                 return;
             }
-            
-            if(newValue != null)
-                this.$emit('input', newValue);
-        }
+
+            if (newValue != null) this.$emit("input", newValue);
+        },
     },
     components: {
         Autocomplete
