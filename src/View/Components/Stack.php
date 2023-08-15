5d6d3ac110f0befc09f2f622f9cc979b69f00e0b
Walter Kimaro <wakyj07@gmail.com>
2023-08-15 13:13:29 +0300

    Add support for row actions to stacks

--- src/View/Components/Stack.php
+++ src/View/Components/Stack.php
@@ -18,6 +18,7 @@ class Stack extends Component
         public $metaField = null,
         public $titleField = null,
         public $descriptionField = null,
+        public $rowActions = null,
         public $sortField = "index",
     ) {
         $this->instanceId = "pierStack" . bin2hex(random_bytes(6));
