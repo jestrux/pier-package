b79fdc492d52e298ec0cd8f69228ac4511b9f27f
Walter Kimaro <wakyj07@gmail.com>
2024-02-04 02:23:24 +0300

    Remove shadow from selected radio option

--- resources/views/components/radio/index.blade.php
+++ resources/views/components/radio/index.blade.php
@@ -3,13 +3,12 @@
 <div x-data="{
     value: '{{ $value ?? '' }}',
     _value: '{{ $value ?? '' }}',
-    choices: {!!$choices!!},
+    choices: {!! $choices !!},
     reset() {
         this.value = '';
         this._value = '';
     },
     init() {
-        console.log({!!$choices!!});
         this.$watch('value', newValue => {
             if (this._value == newValue) return;
 
@@ -30,8 +29,7 @@
     <div class="flex items-center flex-wrap gap-4 -mx-1.5">
         <template x-for="(choice, index) in choices" x-bind:key="index">
             <div x-radio:option x-bind:value="choice.value"
-                class="flex items-center gap-2 cursor-pointer px-2 py-1 rounded focus:outline-none focus:ring-1 ring-content/20"
-                :class="{ 'shadow': $radioOption.isChecked }">
+                class="flex items-center gap-2 cursor-pointer px-2 py-1 rounded focus:outline-none focus:ring-1 ring-content/20">
                 <span :class="$radioOption.isChecked ? 'border-primary' : 'border-content/50'"
                     class="inline-flex p-0.5 h-4 w-4 shrink-0 items-center justify-center rounded-full border"
                     aria-hidden="true">
